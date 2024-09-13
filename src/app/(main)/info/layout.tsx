939448609ae4571cd4dffd882d43a380117a0449
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2024-09-14 01:08:03 +0300

    Fix right side menu width

--- src/app/(main)/info/layout.tsx
+++ src/app/(main)/info/layout.tsx
@@ -12,7 +12,7 @@ export default function InfoLayout({
          <div className="1024p:flex-row flex-col flex w-dvw px-[5%] gap-4">
             <UserInfo
                wrapperClassName="mt-4 1024p:mt-0 1024p:-translate-y-4"
-               className="grid-cols-[repeat(4,250px)] 1024p:grid-cols-1"
+               className="grid-cols-[repeat(4,max-content)] 1024p:grid-cols-1"
             />
             {children}
          </div>
